026a17af4f407845074432c278dbe52791bc98a6
Marcel Marzec <marcelmarzec11@gmail.com>
2026-05-06 20:20:21 +0100

    Basic website, base ios app

---
+++ ios_app/VatSight/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  VatSight
+//
+//  Created by Marcel Marzec on 06/05/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
